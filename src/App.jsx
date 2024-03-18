efce379ec5b068643b3c73c07f23d164c1c60600
Jeymison Menezes <jeymisong@gmail.com>
2024-03-18 16:56:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [peso, setPeso] = useState();
+  const [altura, setAltura] = useState();
+  const [result, setResult] = useState();
+
+  const calcular = () => {
+    const imc = peso / (+altura * +altura)
+    setResult(imc.toFixed(2));
+  }
+  const classificacaoIMC = () => {
+    if (result < 18.5) {
+      return 'Abaixo do peso';
+    } else if (result < 24.9) {
+      return 'Peso normal';
+    } else if (result < 29.9) {
+      return 'Sobrepeso';
+    } else if (result < 34.9) {
+      return 'Obesidade grau 1';
+    } else if (result < 39.9) {
+      return 'Obesidade grau 2';
+    } else {
+      return 'Obesidade grau 3';
+    }
+  };
+
+
+  return (
+    <div className='container'>
+      <text className='text'>Calculadora IMC 2024</text> <br /><br />
+      <label htmlFor="peso">Digite seu Peso (kg): </label>
+      <input  className='input' type="number" value={peso} onChange={(e) => setPeso(Number(e.target.value))} />
+      <label htmlFor="altura">Digite sua altura ex: 1,74: </label>
+      <input className='input' type="number"  value={altura} onChange={(e) => setAltura(Number(e.target.value))}/>
+      <button className='button' type='button' onClick={calcular}>Calcular</button>
+      <h1 className='result'>IMC: {result}</h1>
+      <h3 className='result'>Sua classificação: {classificacaoIMC()}</h3><br />
+    </div>
+  )
+}
+
+export default App
